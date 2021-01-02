f2e3510a24ad0d8a357dea1c459a84ecc8740fe8
luisegarduno <luis.e.garduno@gmail.com>
2021-01-02 09:28:50 -0600

    Finalized minor adjustments

--- README.md
+++ README.md
@@ -2,22 +2,16 @@
 Checks HTML class to see when status changes from out of stock -> in stock. Basic script to run on a Jenkins server and you can switch out the while true statement, or you can just keep it running in the background.
 
 ## Prerequisites:
-- Twilio account & phone number ([see twilio instructions](#twilio-instructions)).  
 - Have the following python3 modules installed: lxml, twilio, requests, beatifulsoup4  
+- Valid (personal/work) phone number
+- Twilio account + Twilio phone number ([see instructions](#instructions)).  
+  - Skip to step #4 of instructions if you already have both of these.
 
-## Twilio Instructions:  
+## Instructions:  
 1.) [Sign up](https://www.twilio.com/try-twilio) or [log in](https://www.twilio.com/login) to Twilio.   
 2.) After account has been confirmed via email, visit your [console](https://www.twilio.com/console) to find your API crendentials (under `project info`).    
 3.) Obtain a phone number via Twilio (2 methods):   
 - Method 1: [Trial](https://www.twilio.com/console/phone-numbers/trial-number/modal?capability[]=sms)  
 - Method 2: [Purchase](https://www.twilio.com/console/phone-numbers/search)  
-
-
-## Instructions:
-1.) Insert your Twilio crendentials within `Client("", "")`   
-- It should look something like this: `client = Client("abc1234","abc1234");`
-
-2.) Insert phone numbers near the bottom of script:   
-- `to=""`    : Phone number you want the text messages to be sent to  
-- `from_=""` : Twilio phone number sending the text messages ([see twilio instructions](#twilio-instructions))  
-
+4.) Insert your Twilio crendentials + Phone Numbers within labeled area   
+5.) Deploy!
--- ThirstBuy.py
+++ ThirstBuy.py
@@ -1,18 +1,23 @@
 import time
 import emoji
-import hashlib
-import requests
 from bs4 import BeautifulSoup
 from twilio.rest import Client
 from urllib.request import urlopen
 
-# Enter Twilio Credentials here 
-client = Client("",                                     # account_sid
-                "")                                     # auth_token
+# -------------- INSERT INFORMATION HERE --------------- #
+# Twilio Credentials
+account_sid = ""
+auth_token  = ""
 
-# Text messages to be sent
-no  = emoji.emojize('RTX 3080: Out of Stock :x:', use_aliases=True)              # Red "X" emoji
-yes = emoji.emojize('RTX 3080: In Stock :white_check_mark:', use_aliases=True)   # Green check mark emoji
+# Phone Numbers
+my_number = ""
+twilio_number = ""
+# ------------------------------------------------------ #
+
+client = Client(account_sid, auth_token)                                        # Twilio Client
+
+no  = emoji.emojize('RTX 3080: Out of Stock :x:', use_aliases=True)             # Out of Stock + Red "X" emoji
+yes = emoji.emojize('RTX 3080: In Stock :white_check_mark:', use_aliases=True)  # In Stock + Green check mark emoji
 
 # Website URL : Microcenter // RTX 3080
 url = 'https://www.microcenter.com/search/search_results.aspx?N=&cat=&Ntt=rtx+3080&searchButton=search'
@@ -21,13 +26,13 @@ while True:
     response = urlopen(url).read()
     soup = BeautifulSoup(response, "lxml")              # A parser tree (lxml format) helps search for a string.
     mydivs = soup.findAll("div", {"class": "stock"})    # Depending on the website, find the field that is changes when the item is in stock.
-    
-    if (str(mydivs).find("in stock") == -1):            # If .find() returns -1, item isn't in stock. 
+
+    if (str(mydivs).find("in stock") == -1):            # If .find() returns -1, item isn't in stock.
         print(no)
 
     else:                                               # Else, item is in stock (send out text message)
-        client.messages.create(to="",                   # YOUR phone number
-                               from_="",                # Twilio phone number
+        client.messages.create(to=my_number,            # YOUR phone number
+                               from_=twilio_number,     # Twilio phone number
                                body=yes)                # Text Message being delivered
 
     time.sleep(120)                                     # Scans website every 2 minutes
