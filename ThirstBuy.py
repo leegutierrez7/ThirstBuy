a3cbdc125ca6d4307df979316b16d3b0cd59ef8a
luisegarduno <luis.e.garduno@gmail.com>
2021-01-22 23:43:56 -0600

    Updated code

--- ThirstBuy.py
+++ ThirstBuy.py
@@ -18,10 +18,13 @@ client = Client(account_sid, auth_token)                    # Twilio Client
 
 item = 'RTX 3080: '
 no  = emoji.emojize(':x:', use_aliases=True)                # Emoji: Red "X"
-yes = emoji.emojize(':white_check_mark:', use_aliases=True) # Emoji: Green check
+yes = emoji.emojize(':white_check_mark: ', use_aliases=True) # Emoji: Green check
 
 # Website URL : Microcenter
-url = 'https://www.microcenter.com/search/search_results.aspx?N=&cat=&Ntt=rtx+3080&searchButton=search'
+# url = 'https://www.microcenter.com/search/search_results.aspx?N=&cat=&Ntt=rtx+3080&searchButton=search'
+url = 'https://www.microcenter.com/search/search_results.aspx?Ntt=rtx+3080&Ntk=all&sortby=match&N=44&myStore=true'
+
+n = 1
 
 while True:
     response = urlopen(url).read()
@@ -31,7 +34,9 @@ while True:
     clearance = soup.findAll("div", {"class": "clearance"})     # Depending on the website, find the field that is changes when the item is in stock.
 
     if (str(online).find("in stock") == -1 or str(clearance).find("$") == -1):  # If .find() returns -1, item isn't in stock.
-        print(item + no)
+        e = emoji.emojize(time.strftime("%-I:%M%p") + ' :satellite: ', use_aliases=True)
+        print(e + str(n) + '\t\t\t' + item + no)
+        n += 1
 
     else:                                                                       # Else, item is in stock (send out text message)
         txt = item 
@@ -44,6 +49,6 @@ while True:
 
         client.messages.create(to=my_number,            # YOUR phone number
                                from_=twilio_number,     # Twilio phone number
-                               body=txt+yes)            # Text Message being delivered
+                               body=txt+yes+url)            # Text Message being delivered
 
     time.sleep(120)                                     # Scans website every 2 minutes
